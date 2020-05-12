c9b4465d76afb85cfb86381e4f45fd51a1a166e9
hai14894 <hohoanghai14894@gmail.com>
2020-05-12 03:34:43 -0700

    style home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :listings
   devise_for :users
-  root to: 'listings#index'
+  get "/", to: "pages#home", as: "root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
