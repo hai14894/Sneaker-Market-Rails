0fea3e5cdda3d98bb59006eef1a58c595692d21c
hai14894 <hohoanghai14894@gmail.com>
2020-05-14 15:52:44 +1000

    add carousel slide on show listing page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -17,7 +17,7 @@ class ListingsController < ApplicationController
 
   # GET /listings/new
   def new
-    @listing = Listing.new
+    @listing = current_user.listings.create
   end
 
   # GET /listings/1/edit
