91a44bf9bb18ccb7f7209d066da43a96c855cde3
hai14894 <hohoanghai14894@gmail.com>
2020-05-18 11:53:48 +1000

    add webhook method to the controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,11 @@
 class PaymentsController < ApplicationController
+    skip_before_action :verify_authenticity_token, only: [:webhook]
+
     def success
 
+    end
+
+    def webhook
+        p params
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   get "/", to: "pages#home", as: "root"
   get "/payments/success", to: "payments#success" #add route for payment success page"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post "/payments/webhook", to: "payments#webhook" 
 end
