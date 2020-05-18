a324e71795540214e3a5eb42778fa77581912388
hai14894 <hohoanghai14894@gmail.com>
2020-05-18 11:55:19 +1000

    fixed webhook method

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,6 +6,14 @@ class PaymentsController < ApplicationController
     end
 
     def webhook
-        p params
+        payment_id= params[:data][:object][:payment_intent]
+    payment = Stripe::PaymentIntent.retrieve(payment_id)
+    listing_id = payment.metadata.listing_id
+    user_id = payment.metadata.user_id
+
+    p "listing id " + listing_id
+    p "user id " + user_id
+
+    status 200
     end
 end
