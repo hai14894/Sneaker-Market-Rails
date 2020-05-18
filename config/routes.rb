a54acb5f3c1767a21933da116382946bd047354b
hai14894 <hohoanghai14894@gmail.com>
2020-05-18 10:33:36 +1000

    add route, controller and view for success purchases

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :listings
   devise_for :users
   get "/", to: "pages#home", as: "root"
+  get "/payments/success", to: "payments#success" #add route for payment success page"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
